cce9ba146667111dd4225bd29de49e6e649ec531
Jacky Ho <jackyho@westside-union.com>
2018-04-27 15:00:41 +0800

    Add files via upload

---
+++ OpenCV_Python3_install_raspberry_pi_3.sh
@@ -0,0 +1,101 @@
+#!/bin/bash
+
+$ sudo apt-get update && sudo apt-get upgrade
+
+$ sudo apt-get install build-essential cmake pkg-config
+
+$ sudo apt-get install libjpeg-dev libtiff5-dev libjasper-dev libpng12-dev
+
+$ sudo apt-get install libavcodec-dev libavformat-dev libswscale-dev libv4l-dev
+$ sudo apt-get install libxvidcore-dev libx264-dev
+
+$ sudo apt-get install libgtk2.0-dev libgtk-3-dev
+
+$ sudo apt-get install libatlas-base-dev gfortran
+
+$ sudo apt-get install python2.7-dev python3-dev
+
+$ cd ~
+$ wget -O opencv.zip https://github.com/Itseez/opencv/archive/3.3.0.zip
+$ unzip opencv.zip
+
+$ cd ~
+$ wget -O opencv.zip https://github.com/Itseez/opencv/archive/3.3.0.zip
+$ unzip opencv.zip
+
+$ cd ~
+$ wget -O opencv.zip https://github.com/Itseez/opencv/archive/3.3.0.zip
+$ unzip opencv.zip
+
+$ cd ~
+$ wget -O opencv.zip https://github.com/Itseez/opencv/archive/3.3.0.zip
+$ unzip opencv.zip
+
+# virtualenv and virtualenvwrapper
+export WORKON_HOME=$HOME/.virtualenvs
+source /usr/local/bin/virtualenvwrapper.sh
+
+$ echo -e "\n# virtualenv and virtualenvwrapper" >> ~/.profile
+$ echo "export WORKON_HOME=$HOME/.virtualenvs" >> ~/.profile
+$ echo "source /usr/local/bin/virtualenvwrapper.sh" >> ~/.profile
+
+$ source ~/.profile
+
+$ mkvirtualenv cv -p python3
+
+$ source ~/.profile
+$ workon cv
+
+$ pip install numpy
+
+$ workon cv
+
+$ cd ~/opencv-3.3.0/
+$ mkdir build
+$ cd build
+$ cmake -D CMAKE_BUILD_TYPE=RELEASE \
+    -D CMAKE_INSTALL_PREFIX=/usr/local \
+    -D INSTALL_PYTHON_EXAMPLES=ON \
+    -D OPENCV_EXTRA_MODULES_PATH=~/opencv_contrib-3.3.0/modules \
+    -D BUILD_EXAMPLES=ON ..
+
+# set size to absolute value, leaving empty (default) then uses computed value
+#   you most likely don't want this, unless you have an special disk situation
+# CONF_SWAPSIZE=100
+CONF_SWAPSIZE=1024
+
+$ sudo /etc/init.d/dphys-swapfile stop
+$ sudo /etc/init.d/dphys-swapfile start
+
+$ make -j4
+
+$ sudo make install
+$ sudo ldconfig
+
+$ ls -l /usr/local/lib/python3.5/site-packages/
+total 1852
+-rw-r--r-- 1 root staff 1895932 Mar 20 21:51 cv2.cpython-34m.so
+
+$ cd /usr/local/lib/python3.5/site-packages/
+$ sudo mv cv2.cpython-35m-arm-linux-gnueabihf.so cv2.so
+
+$ cd ~/.virtualenvs/cv/lib/python3.5/site-packages/
+$ ln -s /usr/local/lib/python3.5/site-packages/cv2.so cv2.so
+
+$ source ~/.profile 
+$ workon cv
+$ python
+>>> import cv2
+>>> cv2.__version__
+'3.3.0'
+>>>
+
+$ rm -rf opencv-3.3.0 opencv_contrib-3.3.0
+
+# set size to absolute value, leaving empty (default) then uses computed value
+#   you most likely don't want this, unless you have an special disk situation
+CONF_SWAPSIZE=100
+# CONF_SWAPSIZE=1024
+
+$ sudo /etc/init.d/dphys-swapfile stop
+$ sudo /etc/init.d/dphys-swapfile start
